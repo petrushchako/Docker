c2840f71c4bf7baf3428a76a6afd183456074991
Alex Petrushchak <petrushchako@gmail.com>
2024-08-15 22:45:11 +0100

    Move spring helloworld app

---
+++ Projects/Java/helloworld-spring/src/main/java/com/example/helloworldspring/HelloworldSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.helloworldspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldSpringApplication.class, args);
+    }
+}
