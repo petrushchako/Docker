454013144cfae112804ffc048cd10ad414f71e39
Alex Petrushchak <petrushchako@gmail.com>
2024-08-17 23:53:20 +0100

    Add comments

--- Projects/JS/react-helloworld/src/App.js
+++ Projects/JS/react-helloworld/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import './App.css';
+// Importing necessary libraries and components
+import React from 'react'; // React is a library for building user interfaces
+import './App.css'; // Importing the CSS file to style the components
 
+// Defining a functional component called App
 function App() {
+  // The return statement defines the JSX (JavaScript XML) to be rendered
   return (
     <div className="App">
+      {/* The <header> element represents the header of the application */}
       <header className="App-header">
-        <h1>Hello, World from React!</h1>
+        {/* Displaying the Hello World message */}
+        <h1>Hello World!</h1>
+        {/* A paragraph element with some additional text */}
+        <p>
+          Welcome to my first React app!
+        </p>
       </header>
     </div>
   );
 }
 
+// Exporting the App component as the default export
+// This allows the App component to be imported and used in other files
 export default App;
--- Projects/JS/react-helloworld/src/index.js
+++ Projects/JS/react-helloworld/src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
-import App from './App';
+// Importing the necessary libraries and components
+import React from 'react'; // React library is required to use JSX and create components
+import ReactDOM from 'react-dom'; // ReactDOM is used to render React components into the DOM
+import './App.css'; // Importing the CSS file to apply global styles to the application
+import App from './App'; // Importing the main App component which contains the application logic
 
+// Rendering the App component into the DOM
 ReactDOM.render(
+  // React.StrictMode is a wrapper component that helps identify potential problems
   <React.StrictMode>
-    <App />
+    <App /> {/* App component is the root component of the application */}
   </React.StrictMode>,
+  // Targeting the DOM element with id 'root' to render the React app
   document.getElementById('root')
 );
