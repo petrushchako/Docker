c0dd9b7e4a8a51531d1734c58fdcf173056a8c72
Alex Petrushchak <petrushchako@gmail.com>
2024-08-18 21:53:39 +0100

    Add Angular app content

---
+++ Projects/JS/angular-helloworld/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Hello World</h1>`,
+  styles: [`h1 { font-family: Lato; }`]
+})
+export class AppComponent { }
