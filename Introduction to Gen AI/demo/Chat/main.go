b05c52fc14733631621fd9af629bfd5cef11ae4e
petrushchako <petrushchako@gmail.com>
2025-08-09 00:33:32 +0100

    Fix .env file access

--- Introduction to Gen AI/demo/Chat/main.go
+++ Introduction to Gen AI/demo/Chat/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-	"context"
+	//	"context"
 	"fmt"
 	"log"
 	"os"
-
-	"github.com/openai/openai-go"
-	"github.com/openai/openai-go/option"
+	// "github.com/openai/openai-go"
+	// "github.com/openai/openai-go/option"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Print(os.Getenv("MODEL_RUNNER_BASE_URL"))
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	value := os.Getenv("MODEL_RUNNER_BASE_URL")
+	fmt.Println("MODEL_RUNNER_BASE_URL:", value)
 }
